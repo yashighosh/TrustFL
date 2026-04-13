1a3d0acd49e4b1298da1e7d9b8060579a359cec1
Yashi Ghosh <yashighosh2019@gmail.com>
2026-04-13 07:11:14 +0530

    feat/update: layout.tsx

--- Frontend/app/(dashboard)/layout.tsx
+++ Frontend/app/(dashboard)/layout.tsx
@@ -7,26 +7,11 @@ import Toast from "@/app/components/ui/Toast";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
   const router = useRouter();
-  
-  let activeTab = 0;
-  if (pathname.includes("/blockchain")) activeTab = 1;
-  if (pathname.includes("/fl")) activeTab = 2;
-
-  const handleTabChange = (idx: number) => {
-    if (idx === 0) router.push("/database");
-    if (idx === 1) router.push("/blockchain");
-    if (idx === 2) router.push("/fl");
-  };
 
   return (
     <ToastProvider>
-      <Navbar
-        activeTab={activeTab}
-        onTabChange={handleTabChange}
-        onLogoClick={() => router.push("/")}
-      />
+      <Navbar onLogoClick={() => router.push("/")} />
       <Toast />
       <div className="min-h-screen pt-16 relative z-[1]">
         {children}
