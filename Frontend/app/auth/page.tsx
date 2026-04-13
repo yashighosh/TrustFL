f673d5beac7bb014e40ed749e9f4bac5071c66fb
Yashi Ghosh <yashighosh2019@gmail.com>
2026-04-13 07:11:14 +0530

    feat/update: page.tsx

--- Frontend/app/auth/page.tsx
+++ Frontend/app/auth/page.tsx
@@ -2,13 +2,20 @@
 
 import { useRouter } from "next/navigation";
 import AuthPage from "@/app/components/auth/AuthPage";
+import { useHospital } from "@/app/context/HospitalContext";
 
 export default function Auth() {
   const router = useRouter();
+  const { setHospital } = useHospital();
+
   return (
-    <AuthPage 
-      onLogin={() => router.push("/database")} 
-      onBack={() => router.push("/")} 
+    <AuthPage
+      onRegister={(hospitalData) => {
+        setHospital(hospitalData);
+        router.push("/database");
+      }}
+      onBack={() => router.push("/")}
+      onSuperAdmin={() => router.push("/admin")}
     />
   );
 }
